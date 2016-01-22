b6a972fc78ec589d7dcdef4bf62eb361961200be
孙滢贺 <sunyinghe@sunyinghedeMac.local>
2016-01-22 03:02:40 -0800

    Initial Commit

---
+++ YHHouseDesign/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHHouseDesign
+//
+//  Created by 孙滢贺 on 16/1/22.
+//  Copyright (c) 2016年 YH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YHHouseDesignTests/YHHouseDesignTests.m
@@ -0,0 +1,40 @@
+//
+//  YHHouseDesignTests.m
+//  YHHouseDesignTests
+//
+//  Created by 孙滢贺 on 16/1/22.
+//  Copyright (c) 2016年 YH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface YHHouseDesignTests : XCTestCase
+
+@end
+
+@implementation YHHouseDesignTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
